a88a6de81be091e1948dadc5e990a5240fba6662
sahilsajan <sahilsajan1@gmail.com>
2025-12-29 22:05:19 -0400

    added q9

---
+++ Easy/9.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int temp = x;
+        int reverse = 0;
+        while(temp != 0){
+            int last = temp % 10;
+            reverse *= 10;
+            reverse += last;
+            temp = temp / 10;
+        }
+        return reverse == x;
+    }
+}
